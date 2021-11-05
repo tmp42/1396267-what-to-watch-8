d3e1520d6b79c667fdcd19c5ad95264c32795bb9
tmp42 <tmp42@mail.ru>
2021-11-05 17:29:30 +0300

    Получение данных со сервера

--- project/src/components/film-info/film-info.tsx
+++ project/src/components/film-info/film-info.tsx
@@ -6,13 +6,26 @@ type FilmInfoProps = {
 
 function FilmInfo({film}: FilmInfoProps): JSX.Element {
   const Starring = film.starring.map((item) => item.concat(', '));
+  const evaluations = ['Bad', 'Normal', 'Good', 'Very good', 'Awesome'];
+  let evaluation: string = '';
+  if (film.rating <= 3) {
+    evaluation = evaluations[0];
+  } else if (film.rating <= 5) {
+    evaluation = evaluations[1];
+  } else if (film.rating <= 8) {
+    evaluation = evaluations[2];
+  } else if (film.rating < 10) {
+    evaluation = evaluations[3];
+  } else {
+    evaluation = evaluations[4];
 
+  }
   return (
     <>
       <div className="film-rating">
         <div className="film-rating__score">{film.rating}</div>
         <p className="film-rating__meta">
-          <span className="film-rating__level">Very good</span>
+          <span className="film-rating__level">{evaluation}</span>
           <span className="film-rating__count">{film.scores_count} ratings</span>
         </p>
       </div>
